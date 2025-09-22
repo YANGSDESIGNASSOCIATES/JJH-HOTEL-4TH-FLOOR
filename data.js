e32411d1c7a40fcd3842cc3b115a069cb5dce9e4
YANGSDESIGNASSOCIATES <shanethuaung101@gmail.com>
2025-09-23 03:30:38 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-14kitchenette-area",
+      "name": "1.4KITCHENETTE AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.117124083951584,
+          "pitch": 0.059406151606701485,
+          "rotation": 0.7853981633974483,
+          "target": "1-24kitchenette-area-toilet"
+        },
+        {
+          "yaw": 1.4855083355645693,
+          "pitch": 0.013634468599111216,
+          "rotation": 0,
+          "target": "4-44bed-room--1"
+        },
+        {
+          "yaw": 0.9041365966482928,
+          "pitch": -0.0020909483830493514,
+          "rotation": 0,
+          "target": "2-34bed-room--2"
+        },
+        {
+          "yaw": 0.8140540556309404,
+          "pitch": 0.18813979286038673,
+          "rotation": 4.71238898038469,
+          "target": "6-64bed-room---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-24kitchenette-area-toilet",
+      "name": "2.4KITCHENETTE AREA TOILET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.737422406468295,
+          "pitch": 0.013646065518910788,
+          "rotation": 0,
+          "target": "0-14kitchenette-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-34bed-room--2",
+      "name": "3.4BED ROOM -2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8664194848089419,
+          "pitch": 0.14808523178835387,
+          "rotation": 13.351768777756625,
+          "target": "0-14kitchenette-area"
+        },
+        {
+          "yaw": 0.903741907781006,
+          "pitch": -0.08604962822125906,
+          "rotation": 1.5707963267948966,
+          "target": "3-34bed-room--2-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-34bed-room--2-toilet",
+      "name": "3.4BED ROOM -2 TOILET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3870455644440103,
+          "pitch": 0.15267047252638832,
+          "rotation": 0,
+          "target": "2-34bed-room--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-44bed-room--1",
+      "name": "4.4BED ROOM -1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0663667130004981,
+          "pitch": -0.027798198677768227,
+          "rotation": 0,
+          "target": "0-14kitchenette-area"
+        },
+        {
+          "yaw": 0.6813296803860833,
+          "pitch": 0.03943841900713352,
+          "rotation": 10.995574287564278,
+          "target": "5-54bed-room---1-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-54bed-room---1-toilet",
+      "name": "5.4BED ROOM - 1 TOILET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0592149570482823,
+          "pitch": 0.08942889236808504,
+          "rotation": 0,
+          "target": "4-44bed-room--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-64bed-room---3",
+      "name": "6.4BED ROOM - 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6090886941508113,
+          "pitch": 0.05380147600601859,
+          "rotation": 1.5707963267948966,
+          "target": "0-14kitchenette-area"
+        },
+        {
+          "yaw": -2.0778171270098067,
+          "pitch": 0.16847205512053876,
+          "rotation": 1.5707963267948966,
+          "target": "7-74bed-room--3-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-74bed-room--3-toilet",
+      "name": "7.4BED ROOM -3 TOILET",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2733326089186914,
+          "pitch": 0.16590665732958243,
+          "rotation": 6.283185307179586,
+          "target": "6-64bed-room---3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "JJH HOTLE 3D (LVL-4)",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
